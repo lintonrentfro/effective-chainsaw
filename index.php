ab3c03fd5dcdef41bd5c4f44765bd0dab44a3479
Linton Rentfro <linton@protonmail.ch>
2016-03-16 21:02:10 -0700

    test

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
 
+function getGuestPage() {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "http://www.dragoncon.org/?q=guests");
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+    $curlRes = curl_exec($ch);
+    curl_close($ch);
+    $data = json_decode($curlRes, true);
+    return $data;
+};
 
-echo "hi";
+function findGuestMatches($rawHTML) {
+	preg_match_all("/guest_details(.+?)b\>/U", $rawHTML, $matches);
+	return $matches;
+};
+
+print findGuestMatches(getGuestPage());
